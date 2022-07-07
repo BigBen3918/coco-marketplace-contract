0c35898269260ff3728695c509de55d3dfed506f
loganworld <akp707d@gmail.com>
2022-07-07 23:20:00 +1000

    update

---
+++ args.js
@@ -0,0 +1,4 @@
+module.exports = [
+    "test1 NFT",
+    "t1NFT"
+];
--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     }
 });
 
+task("check-price", "", async (taskArgs, hre) => {
+    const [owner] = await ethers.getSigners();
+    const Factory = await ethers.getContractFactory("Marketplace");
+    const marketplace = await Factory.attach("0x19c82A352995424Ec6783D18a52c78220A7E60A9");
+    let data = await marketplace.orderByAssetId("0x5017a7f8B89b1eE16DEeE58A8a17Ba4e35060378", "5");
+    console.log(data);
+
+})
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -27,59 +36,59 @@ module.exports = {
             url: "http://127.0.0.1:7545",
         },
         fantom_test: {
-            url: "https://rpc.testnet.fantom.network",
-            accounts: [process.env.PRIVATEKEY,process.env.PRIVATEKEY1],
+            url: "https://ftm-test.babylonswap.finance",//"https://rpc.testnet.fantom.network",
+            accounts: [process.env.PRIVATEKEY, process.env.PRIVATEKEY1],
         },
         ethereum: {
             url: "https://main-light.eth.linkpool.io/",
-            accounts: [process.env.PRIVATEKEY,process.env.PRIVATEKEY1],
+            accounts: [process.env.PRIVATEKEY, process.env.PRIVATEKEY1],
         },
         ICICB: {
             url: "http://3.17.193.52/",
-            accounts: [process.env.PRIVATEKEY,process.env.PRIVATEKEY1],
+            accounts: [process.env.PRIVATEKEY, process.env.PRIVATEKEY1],
         },
         ICICBtestnet: {
             url: "http://13.58.153.103/",
-            accounts: [process.env.PRIVATEKEY,process.env.PRIVATEKEY1],
+            accounts: [process.env.PRIVATEKEY, process.env.PRIVATEKEY1],
         },
         bsc: {
             url: "https://bsc-dataseed1.ninicoin.io/",
-            accounts: [process.env.PRIVATEKEY,process.env.PRIVATEKEY1],
+            accounts: [process.env.PRIVATEKEY, process.env.PRIVATEKEY1],
         },
         matic: {
             url: "https://rpc-mainnet.matic.quiknode.pro",
-            accounts: [process.env.PRIVATEKEY,process.env.PRIVATEKEY1],
+            accounts: [process.env.PRIVATEKEY, process.env.PRIVATEKEY1],
         },
         fantom: {
             url: "https://rpc.ftm.tools/",
-            accounts: [process.env.PRIVATEKEY,process.env.PRIVATEKEY1],
+            accounts: [process.env.PRIVATEKEY, process.env.PRIVATEKEY1],
         },
     },
     etherscan: {
         // Your API key for Etherscan
         // Obtain one at https://etherscan.io/
-        apiKey: "WQKQ9RXXCTK715PKG1H7JUMV4ZEUW3KKXN",
+        apiKey: "CGPDXGVTQXTQVQV4JZ9CTTXUE21IXAGKVB",
     },
     solidity: {
-		compilers: [
-			{
-				version: "0.6.12",
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 200,
-					},
-				}
-			},
-			{
-				version: "0.8.4",
-				settings: {
-					optimizer: {
-						enabled: true,
-						runs: 200,
-					},
-				}
-			},
+        compilers: [
+            {
+                version: "0.6.12",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                }
+            },
+            {
+                version: "0.8.4",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                }
+            },
         ]
     },
     mocha: {
